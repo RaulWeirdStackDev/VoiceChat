7fde9056e7fef238e71ffb831bc9ca0521324997
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-08-15 00:13:30 -0400

    arreglando rutas

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { registerUser } from "../controllers/authController.js";
 
-const router = express.Router();
+export const authRoutes = express.Router();
 
 router.post("/register", registerUser);
 
-export default router;
+
